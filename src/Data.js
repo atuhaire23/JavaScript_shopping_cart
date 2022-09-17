ffafc25477770738f11b5f1b315a9d84c89e1f5f
Atuhaire Felix <atuhaire1738@gmail.com>
2022-09-17 22:17:16 +0300

    Descrptive comment for code changes

---
+++ src/Data.js
@@ -0,0 +1,61 @@
+let shopItemsData = [
+{
+    id: "jfhgbvnss",
+    name: "Crew Neck Shirt",
+    price: 59,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img5.png",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 165,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },  
+  {
+    id: "wuefbncxbsn",
+    name: "Checked Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img9.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 298,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/1.jpg",
+  },
+
+  {
+    id: "gjfjdbb",
+    name: "Men's Suit",
+    price: 450,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img4.jpg",
+  },
+  {
+    id: "hteujdbd",
+    name: "Wedding Suit",
+    price: 190,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img2.jpg",
+  },  
+  {
+    id: "hfjfhfgbf",
+    name: "Biker Shorts",
+    price: 225,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img3.jpg",
+  },
+  {
+    id: "tgbhany",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+  
+  
+];
